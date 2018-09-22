2e34882be12a9f6ee99292abc9a0eff9182770a5
Tian Wong <tian@Tians-MacBook-Pro.local>
2018-09-21 20:33:48 -0700

    Revert "Added space at line 5 to match test"
    This reverts commit 21be316f7ac0852eef23ed38b511fd6301cd1dd8.

--- planets.py
+++ planets.py
@@ -2,7 +2,7 @@ def weight_on_planets():
    # write your code here
    weight = float(input("What do you weigh on Earth? "))
 
-   print("On Mars you would weigh " + str(weight * 0.38) + " pounds. \nOn Jupiter you would weigh " + str(weight * 2.34) + " pounds.")
+   print("On Mars you would weigh " + str(weight * 0.38) + " pounds.\nOn Jupiter you would weigh " + str(weight * 2.34) + " pounds.")
 
 if __name__ == '__main__':
    weight_on_planets()

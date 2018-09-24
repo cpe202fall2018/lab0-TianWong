f5cc70e61cf52459dab6d020d86458d6a5e14866
TianWong <tianwong02@gmail.com>
2018-09-24 16:38:45 -0700

    fixed capitalization of "earth"

--- planets.py
+++ planets.py
@@ -1,6 +1,6 @@
 def weight_on_planets():
    # write your code here
-   weight = float(input("What do you weigh on Earth? "))
+   weight = float(input("What do you weigh on earth? "))
 
    print("On Mars you would weigh " + str(weight * 0.38) + " pounds.\nOn Jupiter you would weigh " + str(weight * 2.34) + " pounds.")
 

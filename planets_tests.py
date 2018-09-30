572bc02d862b29e04d1de0a9f5c12b88a695df45
TianWong <tianwong02@gmail.com>
2018-09-29 22:02:08 -0700

    Fit to test case

--- planets_tests.py
+++ planets_tests.py
@@ -6,8 +6,8 @@ class Test_planets(unittest.TestCase):
 
     def test_01(self):
         sys.stdin = io.StringIO("136")
-        sys.stdout = student_output = io.StringIO()        
-        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds."
+        sys.stdout = student_output = io.StringIO()
+        expected_out = "What do you weigh on earth? On Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds."
         weight_on_planets()
         #print(student_output.getvalue().strip())
         self.assertEqual(expected_out, student_output.getvalue().strip())
